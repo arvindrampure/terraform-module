34f2b2edb8256ef2ecc21eb7b3e02937c00e1025
arvindrampure <95163150+arvindrampure@users.noreply.github.com>
2024-06-06 16:52:43 +0530

    Update main.tf

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -27,6 +27,14 @@ locals {
     #we have define two expression but expression are in key value format
     owners      = local.owners
     environment = local.environment
+resource "azurerm_resource_group" "myrg" {
+  #inside the resource block we provided argument
+  name = "${local.resource_name_prefix}-${var.resource_group_name}"
+  #sap-hr-rg-default
+  ##to call a variable we will use var.nameofthevariables
+  location = var.resource_group_location
+  tags     = local.common_tags
+}
     
   }
 }
